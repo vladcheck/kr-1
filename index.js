5db92b2f7625f720435cf060b0756810fe34035a
Vladimir Valekzhanin <valdemarcheck@yandex.ru>
2025-09-25 09:50:05 +0300

    Finish goods.html

---
+++ index.js
@@ -0,0 +1,12 @@
+document.body.innerHTML =
+  ` <div id="header"><h1>Мой сайт</h1>
+    <nav id="menu">
+        <ul>
+            <li><a href="index.html">Главная</a></li>
+            <li><a href="news.html">Новости</a></li>
+            <li><a href="practices.html">Практики</a></li>
+            <li><a href="goods.html">Товары</a></li>
+            <li><a href="contacts.html">Контакты</a></li>
+        </ul>
+    </nav>
+    </div>` + document.body.innerHTML;
